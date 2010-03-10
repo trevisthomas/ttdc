3acb269353361c3ab0d00d8b642e6c61db0ff1f3
trevisthomas <trevisthomas@db1f2772-1f02-11df-9eab-e12afc24c7b7>
2010-03-10 04:49:06 +0000

    Working on data migration

---  ttdc --username trevisthomas/River/source/Tools/org/ttdc/persistence/migration/EntrySummaryAndTagRemoveForV7.java
+++  ttdc --username trevisthomas/River/source/Tools/org/ttdc/persistence/migration/EntrySummaryAndTagRemoveForV7.java
@@ -1,19 +1,23 @@
 package org.ttdc.persistence.migration;
 
+import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.lang.time.StopWatch;
 import org.apache.log4j.Logger;
 import org.ttdc.biz.network.services.helpers.PostFormatter;
 import org.ttdc.persistence.Persistence;
+import org.ttdc.persistence.objects.AssociationPostTag;
 import org.ttdc.persistence.objects.Entry;
+import org.ttdc.persistence.objects.Post;
 
 public class EntrySummaryAndTagRemoveForV7 {
 	private static Logger log = Logger.getLogger(SortValueOnTag.class);
 	private StopWatch stopwatch = new StopWatch();
 	
-	private void doit(){
+	public void doit(){
 		try{
+			start();
 			log.info("");
 			Persistence.beginSession();
 			int count = 1;
@@ -32,6 +36,9 @@ public class EntrySummaryAndTagRemoveForV7 {
 			Persistence.rollback();
 			log.error(e);
 		}
+		finally{
+			end();
+		}
 	}
 	
 	
@@ -43,5 +50,18 @@ public class EntrySummaryAndTagRemoveForV7 {
 		
 		log.info("Done. "+ thing.stopwatch.toString());
 		System.exit(0);
+	}
+	
+	Date start;
+	Date end;
+
+	public void start(){
+		start = new Date();
+	}
+	public void end(){
+		end = new Date();
+		log.info("Time taken: "+(end.getTime() - start.getTime())/1000.0);
+		log.info("ass count: "+AssociationPostTag.iCount);
+		log.info("post count: "+Post.iCount);
 	}
 }
