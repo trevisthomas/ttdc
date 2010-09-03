88aa055b2c9d0bed9445e8320a4114efb234a0f4
trevisthomas <trevisthomas@db1f2772-1f02-11df-9eab-e12afc24c7b7>
2010-09-03 23:30:25 +0000

    Stated working on tree spacers for the nested comment view.

---
+++  ttdc --username trevisthomas/River/source/Tests/org/ttdc/util/PathToTreeUtilTest.java
@@ -0,0 +1,55 @@
+package org.ttdc.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.ttdc.persistence.objects.Post;
+import static org.junit.Assert.*;
+
+public class PathToTreeUtilTest {
+	@Test
+	public void testCreateArray(){
+		
+		Post p1 = new Post();
+		p1.setPath("00001.00003.00002");
+		
+		Post p2 = new Post();
+		p2.setPath("00003.00001.00012");
+		
+		Post p3 = new Post();
+		p3.setPath("00002.00005");
+		
+		Post p4 = new Post();
+		p4.setPath("00002");
+		
+		List<Post> posts = new ArrayList<Post>();
+		posts.add(p1);
+		posts.add(p2);
+		posts.add(p3);
+		posts.add(p4);
+		
+		PathToTreeUtil util = new PathToTreeUtil();
+		
+		int[] values = util.calculatePathSegmentMaximums(posts);
+		
+		assertEquals(3, values.length);
+		assertEquals(3, values[0]);
+		assertEquals(5, values[1]);
+		assertEquals(12, values[2]);
+	}
+	
+	@Test
+	public void testArraizePath(){
+		PathToTreeUtil util = new PathToTreeUtil();
+		Post p1 = new Post();
+		p1.setPath("00001.00003.00002");
+		int[] values = util.pathToArray(p1);
+		
+		assertEquals(3, values.length);
+		assertEquals(1, values[0]);
+		assertEquals(3, values[1]);
+		assertEquals(2, values[2]);
+		
+	}
+}
