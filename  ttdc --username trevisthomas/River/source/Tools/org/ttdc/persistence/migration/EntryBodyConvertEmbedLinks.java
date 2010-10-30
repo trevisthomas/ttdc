ed80020f883c8c63eaf5f1d1e4a0d8de32e882c0
trevisthomas@gmail.com <trevisthomas@gmail.com@db1f2772-1f02-11df-9eab-e12afc24c7b7>
2010-10-30 23:36:57 +0000

    Header sizing css v6 Embed code added to migration

---
+++  ttdc --username trevisthomas/River/source/Tools/org/ttdc/persistence/migration/EntryBodyConvertEmbedLinks.java
@@ -0,0 +1,71 @@
+package org.ttdc.persistence.migration;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.lang.time.StopWatch;
+import org.apache.log4j.Logger;
+import org.ttdc.gwt.server.beanconverters.ConversionUtils;
+import org.ttdc.persistence.Persistence;
+import org.ttdc.persistence.objects.AssociationPostTag;
+import org.ttdc.persistence.objects.Entry;
+import org.ttdc.persistence.objects.Post;
+
+public class EntryBodyConvertEmbedLinks {
+	private static Logger log = Logger.getLogger(SortValueOnTag.class);
+	private StopWatch stopwatch = new StopWatch();
+	
+	
+	// Disabling this migration step on Oct 25.  I dont want to shacktag in the DB anymore.  
+	// V7 has lost rich text so we're back to pre v7 formatting
+	// also, summaries are no longer needed.
+	
+	public void doit(){
+		try{
+			start();
+			log.info("");
+			Persistence.beginSession();
+			int count = 1;
+			List<Entry> entries = Persistence.session().createQuery("From Entry").list();
+			for (Entry e : entries) {
+				e.setBody(ConversionUtils.fixV6Embed(e.getBody()));
+				Persistence.session().save(e);
+			    
+			    if((count % 1000) == 0)
+			    	Persistence.session().flush();
+			}
+			Persistence.commit();
+		}
+		catch(RuntimeException e){
+			Persistence.rollback();
+			log.error(e);
+		}
+		finally{
+			end();
+		}
+	}
+	
+	
+	public static void main(String[] args) { 
+		EntryBodyConvertEmbedLinks thing = new EntryBodyConvertEmbedLinks();
+		thing.stopwatch.start();
+		thing.doit();
+		thing.stopwatch.stop();
+		
+		log.info("Done. "+ thing.stopwatch.toString());
+		System.exit(0);
+	}
+	
+	Date start;
+	Date end;
+
+	public void start(){
+		start = new Date();
+	}
+	public void end(){
+		end = new Date();
+		log.info("Time taken: "+(end.getTime() - start.getTime())/1000.0);
+		log.info("ass count: "+AssociationPostTag.iCount);
+		log.info("post count: "+Post.iCount);
+	}
+}
