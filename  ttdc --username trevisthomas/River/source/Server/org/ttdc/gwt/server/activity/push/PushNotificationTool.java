f886a74a03bc2dc4333ebd077dd4ecdef8f11371
trevis <trevis@trevis-desktop>
2016-12-17 11:40:24 -0600

    Push notifications!!

---  ttdc --username trevisthomas/River/source/Server/org/ttdc/gwt/server/activity/push/PushNotificationTool.java
+++  ttdc --username trevisthomas/River/source/Server/org/ttdc/gwt/server/activity/push/PushNotificationTool.java
@@ -1,7 +1,11 @@
 package org.ttdc.gwt.server.activity.push;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 import org.hibernate.Query;
@@ -9,33 +13,138 @@ import org.hibernate.Session;
 import org.ttdc.gwt.client.beans.GPost;
 import org.ttdc.gwt.client.messaging.post.PostEvent;
 import org.ttdc.gwt.client.messaging.post.PostEventType;
+import org.ttdc.gwt.shared.util.StringUtil;
 import org.ttdc.persistence.Persistence;
 import org.ttdc.persistence.objects.UserObject;
+import org.ttdc.util.ApplicationProperties;
+
+import com.notnoop.apns.APNS;
+import com.notnoop.apns.ApnsService;
+import com.notnoop.apns.ApnsServiceBuilder;
+
+//application.properties
+//PUSH_CERT=<path to p12 cert>
+//PUSH_PASSWORD=<password of cert>
+//PUSH_ENVIRONMENT=<prod or dev>
 
 public class PushNotificationTool {
+
+	enum Environment {
+		PROD, DEV
+	}
+
 	private final static Logger log = Logger.getLogger(PushNotificationTool.class);
 
-	public void executePushEventCausedBy(String personId, PostEvent event) {
-		// Get all of the device tokens
-		List<UserObject> list = getDeviceTokenUserObjects();
+	private URL certUrl = null;
+	private Environment env = null;
+	private String password = null;
 
-		// Send push notifications to everyone who did not cause the push
-		for (UserObject uo : list) {
-			if (uo.getOwner().getPersonId().equals(personId)) {
-				continue;
+	public PushNotificationTool() {
+		try {
+			String certPath = ApplicationProperties.getAppProperties().getProperty("PUSH_CERT");
+			certUrl = getClass().getClassLoader().getResource(certPath);
+
+			String pushEnv = ApplicationProperties.getAppProperties().getProperty("PUSH_ENVIRONMENT");
+			if ("prod".equals(pushEnv)) {
+				env = Environment.PROD;
+			} else if ("dev".equals(pushEnv)) {
+				env = Environment.DEV;
+			} else {
+				env = null;
 			}
-			if (PostEventType.NEW.equals(event.getType())) {
-				sendPushNotificationPostAdded(uo.getValue(), event.getSource());
-			}
+
+			password = ApplicationProperties.getAppProperties().getProperty("PUSH_PASSWORD");
+			log.debug("Push notification tool initalized!");
+		} catch (IOException e) {
+			log.debug("Push notification tool failed to initalize!");
+			log.error(e);
 		}
 
 	}
 
-	private void sendPushNotificationPostAdded(String deviceToken, GPost gPost) {
-		System.out.println("MOCK! Sending new post " + gPost.getPostId() + "to device token: " + deviceToken);
-
+	public boolean isNotInitialized() {
+		return certUrl == null || env == null || StringUtil.empty(password);
 	}
 
+	private void pushIt(Set<String> deviceTokens, String title, String message) {
+
+		if (isNotInitialized()) {
+			log.warn("Push notifications not sent because PushNotificationTool died.");
+			return;
+		}
+
+		try {
+		ApnsServiceBuilder serviceBuilder = APNS.newService();
+			switch (env) {
+			case PROD: {
+				log.debug("Using Prod push API");
+				serviceBuilder.withCert(certUrl.openStream(), password).withProductionDestination();
+				break;
+			}
+			case DEV: {
+				log.debug("Using Dev push API");
+				serviceBuilder.withCert(certUrl.openStream(), password).withSandboxDestination();
+				break;
+			}
+			default: {
+				throw new RuntimeException("Invalid environment");
+			}
+			}
+
+			ApnsService service = serviceBuilder.build();
+
+			// Payload with custom fields
+			String payload = APNS.newPayload().alertBody(message).alertTitle(title).sound("default")
+					.customField("custom", "custom value").build();
+
+			// //Payload with custom fields
+			// String payload = APNS.newPayload()
+			// .alertBody(message).build();
+
+			// //String payload example:
+			// String payload =
+			// "{\"aps\":{\"alert\":{\"title\":\"My Title 1\",\"body\":\"My message 1\",\"category\":\"Personal\"}}}";
+
+			log.debug("Push: " + payload);
+			for (String deviceToken : deviceTokens) {
+				try {
+					service.push(deviceToken, payload);
+				} catch (Throwable t) {
+					log.error("Failed to push to deviceToken: " + deviceToken, t);
+				}
+			}
+
+		} catch (Throwable t) {
+			certUrl = null; // Disable?
+			log.error("Unrecoverable push notification error. Terminating push.", t);
+		}
+	}
+
+	public void executePushEventCausedBy(String personId, PostEvent event) {
+		// Get all of the device tokens
+		List<UserObject> list = getDeviceTokenUserObjects();
+		Set<String> deviceTokens = new HashSet<String>();
+		// Send push notifications to everyone who did not cause the push
+		for (UserObject uo : list) {
+			if (!uo.getOwner().getPersonId().equals(personId)) {
+				continue;
+			}
+			if (PostEventType.NEW.equals(event.getType())) {
+				deviceTokens.add(uo.getValue());
+			}
+		}
+		if (!deviceTokens.isEmpty()) {
+			sendPushNotificationPostAdded(deviceTokens, event.getSource());
+		}
+	}
+
+	private void sendPushNotificationPostAdded(Set<String> deviceTokens, GPost gPost) {
+		StringBuilder builder = new StringBuilder();
+		builder.append(gPost.getCreator().getLogin()).append(":").append(gPost.getLatestEntry().getSummary());
+		pushIt(deviceTokens, gPost.getTitle(), builder.toString());
+	}
+
+	@SuppressWarnings("unchecked")
 	private List<UserObject> getDeviceTokenUserObjects() {
 		List<UserObject> list = new ArrayList<UserObject>();
 		try {
@@ -53,6 +162,5 @@ public class PushNotificationTool {
 			// throw new ServiceException(t);
 			return list;
 		}
-
 	}
 }
